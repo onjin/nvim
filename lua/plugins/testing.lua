2a2ebddfc0229630033a5c2dfd7d8fd8578f39d4
Marek Wywiał <onjinx@gmail.com>
2022-06-23 10:18:31 +0200

    feat(test): mappings for neotest

--- lua/plugins/testing.lua
+++ lua/plugins/testing.lua
@@ -1,3 +1,5 @@
+local map = require('utils').map
+
 require("neotest").setup({
   adapters = {
     require("neotest-python")({
@@ -6,7 +8,23 @@ require("neotest").setup({
       -- Runner to use. Will use pytest if available by default.
       -- Can be a function to return dynamic value.
       runner = "pytest",
+      executable = "docker-compose exec app pytest --ini=config.ini"
     }),
     require("neotest-plenary")
   }
 })
+
+map('n', '<leader>tn', ':lua require("neotest").run.run()<cr>')
+map('n', '<leader>tl', ':lua require("neotest").run.run_last()<cr>')
+map('n', '<leader>tf', ':lua require("neotest").run.run(vim.fn.expand("%"))<cr>')
+map('n', '<leader>tu', ':lua require("neotest").run.stop()<cr>')
+map('n', '<leader>ta', ':lua require("neotest").run.attach()<cr>')
+
+map('n', '<leader>tw', ':lua require("neotest").summary.toggle()<cr>')
+map('n', '<leader>to', ':lua require("neotest").output.open()<cr>')
+map('n', '<leader>ts', ':lua require("neotest").output.open({ short = true })<cr>')
+
+map('n', ']i', ':lua require("neotest").jump.next()<cr>', { silent = true })
+map('n', '[i', ':lua require("neotest").jump.prev()<cr>', { silent = true })
+map('n', ']n', ':lua require("neotest").jump.next({ status = "failed" })<cr>', { silent = true })
+map('n', '[n', ':lua require("neotest").jump.prev({ status = "failed" })<cr>', { silent = true })
--- plug-config/vim-test.vim
+++ plug-config/vim-test.vim
@@ -11,21 +11,3 @@ let g:VimuxOrientation = "h"
 let g:VimuxHeight = "50"
 
 " these "Ctrl mappings" work well when Caps Lock is mapped to Ctrl
-nmap <Leader>Tn :TestNearest<CR>
-nmap <Leader>Tf :TestFile<CR>
-nmap <Leader>Ta :TestSuite<CR>
-nmap <Leader>Tl :TestLast<CR>
-nmap <Leader>Tg :TestVisit<CR>
-
-" vim-ultest
-"let g:ultest_use_pty = 1
-let test#python#pytest#options = "--color=yes"
-nmap ]t <Plug>(ultest-next-fail)
-nmap [t <Plug>(ultest-prev-fail)
-nmap <Leader>ts :UltestSummary<CR>
-nmap <Leader>tc :UltestClear<CR>
-nmap <Leader>to :UltestOutput<CR>
-
-nmap <Leader>tn :UltestNearest<CR>
-nmap <Leader>tf :Ultest<CR>
-nmap <Leader>tl :UltestLast<CR>
