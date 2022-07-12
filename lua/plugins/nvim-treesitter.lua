19bcd71f38e8b91d03d049f889b52c030f16f816
Marek Wywiał <onjinx@gmail.com>
2022-07-12 15:00:02 +0200

    feat(colors): setup catppuccin

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -21,6 +21,7 @@ require 'nvim-treesitter.configs'.setup {
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
+    use_languagetree = true,
     disable = {}
   },
   indent = {
