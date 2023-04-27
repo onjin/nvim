37dfabae07a9cb4b9d50604441e4cd11a7795902
Marek Wywiał <onjinx@gmail.com>
2023-04-27 09:47:40 +0200

    add glow vimux map

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,27 +1,36 @@
-local function options()
-  return {
-    -- overwrite default configuration
-    -- here, e.g. to enable default bindings
-    copy_sync = {
-      -- enables copy sync and overwrites all register actions to
-      -- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
-      enable = true,
-    },
-    navigation = {
-      -- enables default keybindings (C-hjkl) for normal mode
-      enable_default_keybindings = true,
-    },
-    resize = {
-      -- enables default keybindings (A-hjkl) for normal mode
-      enable_default_keybindings = true,
-    },
-  }
+local function options_tmux()
+    return {
+        -- overwrite default configuration
+        -- here, e.g. to enable default bindings
+        copy_sync = {
+            -- enables copy sync and overwrites all register actions to
+            -- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
+            enable = true,
+        },
+        navigation = {
+            -- enables default keybindings (C-hjkl) for normal mode
+            enable_default_keybindings = true,
+        },
+        resize = {
+            -- enables default keybindings (A-hjkl) for normal mode
+            enable_default_keybindings = true,
+        },
+    }
 end
-local function config()
-	local p = require("tmux")
-	p.setup(options())
+
+local function config_tmux()
+    local p = require("tmux")
+    p.setup(options_tmux())
 end
+
+vim.keymap.set('n', '<leader>vg', ':call VimuxRunCommand("clear; glow " . bufname("%"))<CR>')
+
 return {
-  "aserowy/tmux.nvim",
-  config = config,
+    {
+        "aserowy/tmux.nvim",
+        config = config_tmux,
+    },
+    {
+        "preservim/vimux",
+    },
 }
