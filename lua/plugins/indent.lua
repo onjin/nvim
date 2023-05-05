4f05b369897f72fddaa866443821aadca02c8ff6
Marek Wywiał <onjinx@gmail.com>
2023-05-05 10:42:20 +0200

    change listchars

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,17 +1,21 @@
 vim.opt.list = true
 vim.opt.listchars:append "space: "
-vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append "tab:▸ "
+vim.opt.listchars:append "eol:¬"
+vim.opt.listchars:append "trail:●"
+vim.opt.listchars:append "precedes:«"
+vim.opt.listchars:append "extends:»"
 
 return {
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    lazy = false,
-    config = function()
-      require("indent_blankline").setup {
-        space_char_blankline = " ",
-        show_current_context = true,
-        show_current_context_start = true,
-      }
-    end
-  }
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        lazy = false,
+        config = function()
+            require("indent_blankline").setup({
+                space_char_blankline = " ",
+                show_current_context = true,
+                show_current_context_start = true,
+            })
+        end,
+    },
 }
