624ef95691f6a515be4b39c17d1fd7c42970ac1b
Marek Wywiał <onjinx@gmail.com>
2024-03-07 11:56:32 +0100

    add silicone code snapshot

---
+++ lua/plugins/codeshot.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "michaelrommel/nvim-silicon",
+        lazy = true,
+        cmd = "Silicon",
+        config = function()
+            require("silicon").setup({
+                -- Configuration here, or leave empty to use defaults
+                font = "VictorMono NF=34;Noto Color Emoji=34",
+                to_clipboard = true,
+            })
+        end,
+    },
+}
