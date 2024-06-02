f75312215510cdabc72d591fd7ab9106398112a4
Marek Wywiał <onjinx@gmail.com>
2024-06-02 19:05:51 +0200

    disable tailwind-tools - discrupts other lsps

--- lua/custom/plugins/tailwindcss.lua
+++ lua/custom/plugins/tailwindcss.lua
@@ -1,14 +1,16 @@
-return {
-  "onjin/tailwind-tools.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  opts = {
-    document_color = {
-      enabled = true,
-      kind = "inline",
-    },
-    conceal = {
-      enabled = true,
-      min_length = 15,
-    },
-  }, -- your configuration
-}
+return {}
+-- return {
+--   "onjin/tailwind-tools.nvim",
+--   event = "VeryLazy",
+--   dependencies = { "nvim-treesitter/nvim-treesitter" },
+--   opts = {
+--     document_color = {
+--       enabled = true,
+--       kind = "inline",
+--     },
+--     conceal = {
+--       enabled = true,
+--       min_length = 15,
+--     },
+--   }, -- your configuration
+-- }
