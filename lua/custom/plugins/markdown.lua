a8f8a3e1c8f303c6ae996240a91b234a1b073dfc
Marek Wywiał <onjinx@gmail.com>
2024-07-23 12:21:21 +0200

    fix: add npm install neovim to markdown preview plugin

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,10 +1,21 @@
 return {
+  -- install with yarn or npm
   {
     "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
+    build = "cd app && npm install neovim && npm install",
     config = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end,
     ft = { "markdown" },
   },
+
+  -- install without yarn or npm
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --   ft = { "markdown" },
+  --   build = function()
+  --     vim.fn["mkdp#util#install"]()
+  --   end,
+  -- },
 }
