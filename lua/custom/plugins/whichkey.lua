86eceaaeec80690e05e110ae91a54862b11e5cd0
Marek Wywiał <onjinx@gmail.com>
2024-07-29 23:03:45 +0200

    disable which notify with warnings

--- lua/custom/plugins/whichkey.lua
+++ lua/custom/plugins/whichkey.lua
@@ -2,7 +2,7 @@ return { -- Useful plugin to show you pending keybinds.
   "folke/which-key.nvim",
   event = "VimEnter", -- Sets the loading event to 'VimEnter'
   config = function() -- This is the function that runs, AFTER loading
-    require("which-key").setup()
+    require("which-key").setup { notify = false }
 
     -- Document existing key chains
     require("which-key").register {
