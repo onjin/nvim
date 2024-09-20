931ee267951cab775a0313acb08c85c5dfcbb0c0
Marek Wywiał <onjinx@gmail.com>
2024-09-20 15:31:34 +0200

    fix: upgrade packages, cause basedpyright was not working, disabled barbecue temporary cause its not working

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,6 +1,13 @@
 return {
   {
+    "SmiteshP/nvim-navic",
+  },
+  {
+    "nvim-tree/nvim-web-devicons", -- optional dependency
+  },
+  {
     "utilyre/barbecue.nvim",
+    enabled = false,
     name = "barbecue",
     version = "*",
     dependencies = {
