f7cd423554d1c82e2d8d81e637d56efae0ece5d8
Marek Wywiał <onjinx@gmail.com>
2024-09-29 00:13:32 +0200

    feat: add kubectl plugin

---
+++ lua/custom/plugins/k8s.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "ramilito/kubectl.nvim",
+    config = function()
+      require("kubectl").setup()
+      vim.keymap.set("n", "<leader>k", '<cmd>lua require("kubectl").toggle()<cr>', { noremap = true, silent = true })
+    end,
+  },
+}
