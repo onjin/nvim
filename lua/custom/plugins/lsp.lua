e9ad473d2aee694fe4d139b36c7ce95db1b15f80
Marek Wywiał <onjinx@gmail.com>
2024-09-30 12:10:33 +0200

    feat: add lsp outline

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -346,4 +346,15 @@ return {
       end, { desc = "LSP: [C]ode [F]ormat" })
     end,
   },
+  {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+    keys = { -- Example mapping to toggle outline
+      { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    },
+    opts = {
+      -- Your setup opts here
+    },
+  },
 }
