20afc685cfb4f928346c5c88b717e761791fb062
Marek Wywiał <onjinx@gmail.com>
2024-11-06 11:08:55 +0100

    feat: add remote container attach configuration for dap python

--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -15,6 +15,7 @@ return {
   },
   {
     "mfussenegger/nvim-dap-python",
+    dependencies = { "mfussenegger/nvim-dap" },
     ft = "python",
     config = function()
       require("dap-python").setup(vim.g.python_host_prog)
@@ -25,6 +26,30 @@ return {
         require("dap-python").test_class()
       end, { desc = "[D]AP UI [T]est [C]lass" })
       require("dap-python").test_runner = "pytest"
+
+      table.insert(require("dap").configurations.python, {
+        type = "python",
+        request = "attach",
+        name = "Container Python Attach",
+        port = function()
+          return vim.fn.input("Port > ", 3001)
+        end,
+        host = function()
+          return vim.fn.input("Host > ", "127.0.0.1")
+        end,
+        mode = "remote",
+        cwd = vim.fn.getcwd(),
+        pathMappings = {
+          {
+            localRoot = function()
+              return vim.fn.input("Local code folder > ", vim.fn.getcwd() .. "/src", "file")
+            end,
+            remoteRoot = function()
+              return vim.fn.input("Container code folder > ", "/opt/application", "file")
+            end,
+          },
+        },
+      })
     end,
   },
   {
