0bfa28112b1c16d435b4a18f19e7ed6f8c2fa451
Marek Wywiał <onjinx@gmail.com>
2024-12-10 16:00:26 +0100

    fix treesitter settings - still, after upgrade need to cleanup local state/share installation

--- lua/custom/snippets/lua.lua
+++ lua/custom/snippets/lua.lua
@@ -24,4 +24,12 @@ ls.add_snippets("lua", {
       { event = i(1, "event"), group = i(2, "group"), body = i(3, "") }
     )
   ),
+  s(
+    "autogroup",
+    fmt([[vim.api.nvim_create_augroup("{group}", {{ clear = true }}){nl}]], { group = i(1, "group"), nl = i(2, "") })
+  ),
+  s("win", fmt([[local win = vim.api.nvim_get_current_win()]], {})),
+  s("buf", fmt([[local buf = vim.api.nvim_get_current_buf()]], {})),
+  s("bufvalid", fmt([[vim.api.nvim_buf_is_valid({buf})]], { buf = i(1, "buf") })),
+  s("winvalid", fmt([[vim.api.nvim_win_is_valid({win})]], { win = i(1, "win") })),
 })
--- lua/custom/treesitter.lua
+++ lua/custom/treesitter.lua
@@ -7,12 +7,16 @@ M.setup = function()
 
   require("nvim-treesitter.configs").setup {
     ensure_installed = vim.g.treesitter_ensure_installed,
-    sync_install = false,
+    -- Autoinstall languages that are not installed
     auto_install = true,
-    ignore_install = {},
     highlight = {
       enable = true,
+      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+      --  If you are experiencing weird indenting issues, add the language to
+      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+      additional_vim_regex_highlighting = { "ruby" },
     },
+    indent = { enable = true, disable = { "ruby" } },
   }
   vim.opt.foldmethod = "expr"
   vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
