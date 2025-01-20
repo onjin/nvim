4d53ecdf619d174b66abeef338ec59d8406cc26e
Marek Wywiał <onjinx@gmail.com>
2025-01-20 07:46:53 +0100

    add navi syntax

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -138,3 +138,9 @@ local disabled_built_ins = {
 for _, plugin in pairs(disabled_built_ins) do
   vim.g["loaded_" .. plugin] = 1
 end
+
+vim.filetype.add {
+  extensions = {
+    cheat = "navi",
+  },
+}
