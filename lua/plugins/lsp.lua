3d41a1beb4a50fc894dc8a75b5da1416ffef0383
Marek Wywiał <onjinx@gmail.com>
2025-01-24 16:49:38 +0100

    add cargo and trouble plugins

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -281,4 +281,21 @@ return {
       -- Your setup opts here
     },
   },
+  {
+    "saecki/crates.nvim",
+    tag = "stable",
+    config = function()
+      require("crates").setup {
+        lsp = {
+          enabled = true,
+          on_attach = function(client, bufnr)
+            -- the same on_attach function as for your other lsp's
+          end,
+          actions = true,
+          completion = true,
+          hover = true,
+        },
+      }
+    end,
+  },
 }
