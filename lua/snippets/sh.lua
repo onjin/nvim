b7090e28e30f9ddf2915c7d8dfa1267e813b5327
Marek Wywiał <onjinx@gmail.com>
2025-02-25 10:42:06 +0100

    feat: generate all shell color codes variables snippets

--- lua/snippets/sh.lua
+++ lua/snippets/sh.lua
@@ -9,9 +9,7 @@ local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets("sh", {
   s("set#safe", fmt([[set -Eeuo pipefail]], {})),
-
   s("$script_dir", fmt([[script_dir=$(cd "$(dirname "${{BASH_SOURCE[0]}}")" &>/dev/null && pwd -P)]], {})),
-
   s(
     "trap#close",
     fmt(
@@ -44,15 +42,15 @@ setup_colors() {{
   if test -t 2; then
     if test -z "${{NO_COLOR-}}"; then
         if test "${{TERM-}}" != "dumb"; then
-            NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+            NC='\e[0m' RED='\e[31m' GREEN='\e[32m' ORANGE='\e[33m' BLUE='\e[34m' PURPLE='\e[35m' CYAN='\e[36m' YELLOW='\e[0;33m'
         else
-            NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+            NC='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
         fi
     else
-        NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+        NC='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
     fi
   else
-      NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+      NC='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
   fi
 }}
 
@@ -109,3 +107,90 @@ setup_colors
     )
   ),
 })
+local text_types = {
+  normal = 0,
+  bold = 1,
+  faint = 2,
+  italics = 3,
+  underline = 4,
+}
+local fg_colors = {
+  black = 30,
+  red = 31,
+  green = 32,
+  yellow = 33,
+  blue = 34,
+  magenta = 35,
+  cyan = 36,
+  light_gray = 37,
+  gray = 90,
+  light_red = 91,
+  light_green = 92,
+  light_yellow = 93,
+  light_blue = 94,
+  light_magenta = 95,
+  light_cyan = 96,
+  white = 97,
+}
+local bg_colors = {
+  black = 40,
+  red = 41,
+  green = 43,
+  yellow = 43,
+  blue = 44,
+  magenta = 45,
+  cyan = 46,
+  light_gray = 47,
+  gray = 100,
+  light_red = 101,
+  light_green = 102,
+  light_yellow = 103,
+  light_blue = 104,
+  light_magenta = 105,
+  light_cyan = 106,
+  white = 107,
+}
+
+local snippets = {}
+
+-- create foreground color snippets like
+-- - direct color code: GREEN -> '\\e[..'
+-- - color code variable: GREEN= -> GREEN='\\e[..'
+for color, color_code in pairs(fg_colors) do
+  for text_type, text_type_code in pairs(text_types) do
+    local suffix
+    if text_type_code == 0 then
+      suffix = ""
+    else
+      suffix = "_" .. string.upper(text_type)
+    end
+    local snippet_name = string.upper(color) .. suffix
+    local escaped_code = "\\e[" .. text_type_code .. ";" .. color_code .. "m"
+
+    -- inserts variable COLOR_TYPE='\e[...m'
+    table.insert(snippets, s(snippet_name .. "=", fmt(snippet_name .. "='" .. escaped_code .. "'", {})))
+
+    -- inserts color code directly
+    table.insert(snippets, s(snippet_name, fmt(escaped_code, {})))
+  end
+end
+
+-- create background color snippets like
+-- - direct color code: GREEN_BG -> '\\e[..'
+-- - color code variable: GREEN_BG= -> GREEN_BG='\\e[..'
+for color, color_code in pairs(bg_colors) do
+  local snippet_name = string.upper(color) .. "_BG"
+  local escaped_code = "\\e[" .. color_code .. "m"
+
+  -- inserts variable COLOR_TYPE='\e[...m'
+  table.insert(snippets, s(snippet_name .. "=", fmt(snippet_name .. "='" .. escaped_code .. "'", {})))
+
+  -- inserts color code directly
+  table.insert(snippets, s(snippet_name, fmt(escaped_code, {})))
+end
+
+-- add no color variables
+table.insert(snippets, s("NC", fmt("\\e[0m", {})))
+table.insert(snippets, s("NC=", fmt("NC='\\e[0m'", {})))
+
+ls.add_snippets("sh", snippets)
