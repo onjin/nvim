f717b1decf299b1fa14f63c3d117647060ff8705
Marek Wywiał <onjinx@gmail.com>
2025-03-16 22:10:47 +0100

    fix: update java syntax

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,5 +1,10 @@
+local set = vim.opt_local
+
+set.shiftwidth = 4
+set.expandtab = true
+
 local config = {
   cmd = { vim.fn.expand "~/.local/share/nvim/mason/bin/jdtls" },
-  root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+  root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw", "pom.xml" }, { upward = true })[1]),
 }
 require("jdtls").start_or_attach(config)
