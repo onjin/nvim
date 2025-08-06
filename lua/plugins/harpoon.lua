d986e4f5e3dfe6c7f67c5e216ebbaf3e711c87ec
Marek Wywiał <onjinx@gmail.com>
2025-08-06 10:55:54 +0200

    feat: add C-d mapping to remove harpooned files using telescope

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,14 +13,33 @@ return {
         table.insert(file_paths, item.value)
       end
 
+      local finder = function()
+        local paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(paths, item.value)
+        end
+
+        return require("telescope.finders").new_table({
+          results = paths,
+        })
+      end
       require("telescope.pickers")
           .new({}, {
             prompt_title = "Harpoon",
-            finder = require("telescope.finders").new_table {
-              results = file_paths,
-            },
+            finder = finder(),
             previewer = conf.file_previewer {},
             sorter = conf.generic_sorter {},
+            attach_mappings = function(prompt_bufnr, map)
+              map("i", "<C-d>", function()
+                local state = require("telescope.actions.state")
+                local selected_entry = state.get_selected_entry()
+                local current_picker = state.get_current_picker(prompt_bufnr)
+
+                table.remove(harpoon_files.items, selected_entry.index)
+                current_picker:refresh(finder())
+              end)
+              return true
+            end,
           })
           :find()
     end
