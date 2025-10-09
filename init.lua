73c4326eb88c06eef601eb83867ff320a7884622
Marek Wywiał <onjinx@gmail.com>
2025-10-09 21:25:58 +0200

    feat: hide nvim-lspconfig deprecation message

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
+local _deprecate = vim.deprecate
+
+vim.deprecate = function(name, alternative, version, plugin, backtrace)
+  -- hide deprecation info about migration to vim.lsp.config
+  if plugin ~= "nvim-lspconfig" then
+    _deprecate(name, alternative, version, plugin, backtrace)
+  end
+end -- hide nvim-lspconfig alert
+
 require "config.options"
 require "config.commands"
 require "config.keymaps"
