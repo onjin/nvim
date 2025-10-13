b1c34088e3fe2da5328bb4fd397f848b2dce08cc
Marek Wywiał <onjinx@gmail.com>
2025-10-13 22:37:46 +0200

    feat(plugins): add harpoon

--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -8,6 +8,8 @@
 --   config   - if there is not `setup()` method or to customize setup
 --   pin      - optional ref/branch/tag/commit
 --   event    - lazy.nvim specific level (np. "VeryLazy" / "BufReadPre"), ignore in others (FIXME)
+--
+local setkey = require('utils').setkey
 
 -- Mini plugins - list of mini plugin names to enable - to lazy to write separate add/require/setup
 local minis_enabled = {
@@ -86,7 +88,39 @@ local M = {
     },
 
     -- mini-git - has a 'dash' instead of 'dot' so it is not on a 'minis_enabled' list
-    { source = 'nvim-mini/mini-git', name = 'mini.git', stage = 'later' },
+    { source = 'nvim-mini/mini-git',           name = 'mini.git', stage = 'later' },
+
+    -- code outline side window
+    {
+        source = 'stevearc/aerial.nvim',
+        name = 'aerial',
+        stage = 'later',
+        config = function()
+            require('aerial').setup({
+                on_attach = function(bufnr)
+                    vim.keymap.set("n", "{", "<cmd>AerialPrev<cr>", { buffer = bufnr })
+                    vim.keymap.set("n", "]", "<cmd>AerialNext<cr>", { buffer = bufnr })
+                end
+            })
+            setkey("n", "<leader>oo", "<cmd>AerialToggle!<cr>", "Open code outline (aerial)")
+        end
+    },
+
+    -- snippets colletion
+    { source = 'rafamadriz/friendly-snippets', stage = 'later' },
+
+    -- Generate docstrings using :Neogen
+    { source = 'danymat/neogen',               stage = 'later' },
+
+    -- Harpoon2 - it is nice to jump between marked files
+    {
+        source = 'ThePrimeagen/harpoon',
+        pin = { checkout = 'harpoon2' },
+        depends = {
+            { source = 'nvim-lua/plenary.nvim' },
+        },
+        config = require("plugins.config.harpoon").config
+    },
 }
 
 for _, mod in ipairs(minis_enabled) do
@@ -96,14 +130,60 @@ for _, mod in ipairs(minis_enabled) do
         name = "mini." .. mod,
         stage = "later", --
         config = (mod == "hipatterns") and function()
-            local hipatterns = require("mini.hipatterns")
+                local hipatterns = require("mini.hipatterns")
 
-            hipatterns.setup({
-                highlighters = {
-                    hex_color = hipatterns.gen_highlighter.hex_color(),
-                },
-            })
-        end or nil,
+                hipatterns.setup({
+                    highlighters = {
+                        hex_color = hipatterns.gen_highlighter.hex_color(),
+                    },
+                })
+            end or (mod == 'icons') and function()
+                require('mini.icons').tweak_lsp_kind()
+                require('mini.icons').mock_nvim_web_devicons()
+            end or (mod == "snippets") and function()
+                local gen_loader = require('mini.snippets').gen_loader
+                require('mini.snippets').setup({
+                    snippets = {
+                        -- Load snippets based on current language by reading files from
+                        -- "snippets/" subdirectories from 'runtimepath' directories.
+                        gen_loader.from_lang(),
+                    },
+                })
+                require('mini.snippets').start_lsp_server() -- add snippets to LSP engine / completion
+            end or (mod == "notify") and function()
+                require('mini.notify').setup({
+                    -- Content management
+                    content = {
+                        -- Function which formats the notification message
+                        -- By default prepends message with notification time
+                        format = nil,
+                        -- Function which orders notification array from most to least important
+                        -- By default orders first by level and then by update timestamp
+                        sort = nil,
+                    },
+
+                    -- Notifications about LSP progress
+                    lsp_progress = {
+                        -- Whether to enable showing
+                        enable = false,
+                        -- Notification level
+                        level = 'INFO',
+                        -- Duration (in ms) of how long last message should be shown
+                        duration_last = 1000,
+                    },
+                    -- Window options
+                    window = {
+                        -- Floating window config
+                        config = {},
+                        -- Maximum window width as share (between 0 and 1) of available columns
+                        max_width_share = 0.382,
+                        -- Value of 'winblend' option
+                        winblend = 25,
+                    },
+
+                })
+            end or
+            nil,
     })
 end
 
