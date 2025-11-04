21f0dba8ab3be93fababba6e71197f531175e676
Marek Wywiał <onjinx@gmail.com>
2025-11-04 21:45:38 +0100

    fix(cord): fix theme config

--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -369,35 +369,35 @@ local M = {
     {
         'vyfor/cord.nvim',
         build = ':Cord update',
-        config = function()
-            require('cord').setup {
-                log_level = vim.log.levels.WARN,
-                enabled = true,
+        opts = {
+            log_level = vim.log.levels.INFO,
+            enabled = true,
+            display = {
                 theme = 'catppuccin',
                 flavor = 'accent',
-                advanced = {
-                    discord = {
-                        pipe_paths = { '/run/user/' .. string.format("%s", io.popen('id -u')) .. 'discord-ipc-0' },
-                        reconnect = {
-                            enabled = true,
-                        }
-                    },
+            },
+            advanced = {
+                discord = {
+                    pipe_paths = { '/run/user/' .. string.format("%s", io.popen('id -u')) .. 'discord-ipc-0' },
+                    reconnect = {
+                        enabled = true,
+                    }
                 },
-                plugins = {
-                    ['cord.plugins.visibility'] = {
-                        precedence = 'blacklist',
-                        rules = {
-                            blacklist = {
-                                '~/Workspace/p/cint', -- matches path
-                                { type = 'glob', value = '**/sops/**' },
-                                -- function example
-                                function(ctx) return ctx.workspace == 'secret' end,
-                            },
+            },
+            plugins = {
+                ['cord.plugins.visibility'] = {
+                    precedence = 'blacklist',
+                    rules = {
+                        blacklist = {
+                            '~/Workspace/p/cint', -- matches path
+                            { type = 'glob', value = '**/sops/**' },
+                            -- function example
+                            function(ctx) return ctx.workspace == 'secret' end,
                         },
                     },
-                }
+                },
             }
-        end
+        }
     },
 }
 
