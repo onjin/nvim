1475b2f66e38d91e840e468fb4b9be36f57b2ccc
Marek Wywiał <onjinx@gmail.com>
2025-11-22 00:56:40 +0100

    feat(autocomplete): replace cmp with blink, integrate with codeium

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,10 +1,7 @@
 local M = {}
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if has_cmp then
-    capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
-end
+capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities({}, false))
 local default_position_encoding = "utf-16"
 capabilities.general = capabilities.general or {}
 capabilities.general.positionEncodings = { default_position_encoding }
--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -82,15 +82,66 @@ local M = {
 
     -- Completion engine
     {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-buffer",
-            "abeldekat/cmp-mini-snippets",
-        },
-        config = require('plugins.config.cmp'),
+        'saghen/blink.cmp',
+        -- optional: provides snippets for the snippet source
+        dependencies = { 'rafamadriz/friendly-snippets' },
+
+        -- use a release tag to download pre-built binaries
+        version = '1.*',
+        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+        -- build = 'cargo build --release',
+        -- If you use nix, you can build from source using latest nightly rust with:
+        -- build = 'nix run .#build-plugin',
+
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        ---
+
+        config = function()
+            opts = {
+                -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+                -- 'super-tab' for mappings similar to vscode (tab to accept)
+                -- 'enter' for enter to accept
+                -- 'none' for no mappings
+                --
+                -- All presets have the following mappings:
+                -- C-space: Open menu or open docs if already open
+                -- C-n/C-p or Up/Down: Select next/previous item
+                -- C-e: Hide menu
+                -- C-k: Toggle signature help (if signature.enabled = true)
+                --
+                -- See :h blink-cmp-config-keymap for defining your own keymap
+                keymap = { preset = 'default' },
+
+                appearance = {
+                    -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                    -- Adjusts spacing to ensure icons are aligned
+                    nerd_font_variant = 'mono'
+                },
+
+                -- (Default) Only show the documentation popup when manually triggered
+                completion = { documentation = { auto_show = false } },
+
+                -- Default list of enabled providers defined so that you can extend it
+                -- elsewhere in your config, without redefining it, due to `opts_extend`
+                sources = {
+                    default = { 'lsp', 'path', 'snippets', 'buffer' },
+                },
+
+                -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+                -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+                -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+                --
+                -- See the fuzzy documentation for more information
+                fuzzy = { implementation = "prefer_rust_with_warning" }
+            }
+            if vim.g.ai_enabled then
+                table.insert(opts.sources.default, 'codeium')
+                opts.sources.providers = { codeium = { name = 'Codeium', module = 'codeium.blink', async = true } }
+            end
+            require('blink.cmp').setup(opts);
+        end,
+        opts_extend = { "sources.default" }
     },
 
     -- Generate docstrings using :Neogen
@@ -380,6 +431,7 @@ local M = {
             if vim.g.ai_enabled then
                 require("codeium").setup({
                     virtual_text = { enabled = false },
+                    enable_cmp_source = false,
                 })
             end
         end,
