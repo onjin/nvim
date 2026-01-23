43f2b82cfdcef3bbfe5df99e417c4e9cdfb9326c
Marek Wywiał <onjinx@gmail.com>
2026-01-23 11:40:29 +0100

    feat: removed which-key and configure miniclue

--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -50,18 +50,6 @@ local M = {
     -- TODO/FIXME & other tags highlightning
     { 'folke/todo-comments.nvim',     opts = require('plugins.config.todo-comments') },
 
-    -- Show menu with mappings
-    {
-        'folke/which-key.nvim',
-        lazy = false,
-        config = function()
-            vim.keymap.set("n", "<leader>?", function() require("which-key").show({ global = false }) end,
-                { desc = "Buffer Local Keymaps (which-key)" })
-            vim.keymap.set("n", "<leader>/", function() require("which-key").show({ global = true }) end,
-                { desc = "Buffer Local Keymaps (which-key)" })
-        end
-    },
-
     -- code outline side window
     {
         'stevearc/aerial.nvim',
@@ -164,7 +152,50 @@ local M = {
     {
         "nvim-mini/mini.clue",
         name = "mini.clue",
-        opts = {},
+        config = function()
+            local miniclue = require('mini.clue')
+            miniclue.setup({
+                triggers = {
+                    -- Leader triggers
+                    { mode = { 'n', 'x' }, keys = '<Leader>' },
+
+                    -- `[` and `]` keys
+                    { mode = 'n',          keys = '[' },
+                    { mode = 'n',          keys = ']' },
+
+                    -- Built-in completion
+                    { mode = 'i',          keys = '<C-x>' },
+
+                    -- `g` key
+                    { mode = { 'n', 'x' }, keys = 'g' },
+
+                    -- Marks
+                    { mode = { 'n', 'x' }, keys = "'" },
+                    { mode = { 'n', 'x' }, keys = '`' },
+
+                    -- Registers
+                    { mode = { 'n', 'x' }, keys = '"' },
+                    { mode = { 'i', 'c' }, keys = '<C-r>' },
+
+                    -- Window commands
+                    { mode = 'n',          keys = '<C-w>' },
+
+                    -- `z` key
+                    { mode = { 'n', 'x' }, keys = 'z' },
+                },
+
+                clues = {
+                    -- Enhance this by adding descriptions for <Leader> mapping groups
+                    miniclue.gen_clues.square_brackets(),
+                    miniclue.gen_clues.builtin_completion(),
+                    miniclue.gen_clues.g(),
+                    miniclue.gen_clues.marks(),
+                    miniclue.gen_clues.registers(),
+                    miniclue.gen_clues.windows(),
+                    miniclue.gen_clues.z(),
+                },
+            })
+        end
     },
     {
         "nvim-mini/mini.cursorword",
