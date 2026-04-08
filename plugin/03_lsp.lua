7f8a6c0e315e96bd2f0e3395e68fec3125d6e0e9
Marek Wywiał <onjinx@gmail.com>
2026-04-08 14:11:55 +0200

    feat(jdtls): detect if workspace has a `eclipse.formatter.xml` and tell jdtls to use it

--- plugin/03_lsp.lua
+++ plugin/03_lsp.lua
@@ -3,6 +3,24 @@ vim.pack.add { { src = "https://github.com/folke/lazydev.nvim", confirm = false 
 
 require("lazydev").setup()
 
+local function jdtls_formatter_url(bufnr, client)
+  -- detect workspace if `eclipse.formatter.xml` exists
+  -- and returns full path to it
+  local root = client.config.root_dir
+  if not root then
+    local bufname = vim.api.nvim_buf_get_name(bufnr)
+    root = vim.fs.root(bufname, { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle", "build.gradle.kts" })
+  end
+  if not root then
+    return nil
+  end
+
+  local formatter = vim.fs.joinpath(root, "eclipse.formatter.xml")
+  if vim.fn.filereadable(formatter) == 1 then
+    return vim.uri_from_fname(formatter)
+  end
+end
+
 ---@type table<string, vim.lsp.Config|true>
 local servers = {
   jdtls = {
@@ -43,6 +61,22 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
     if client:supports_method "textDocument/inlayHint" then
       vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+    end
+    if client.name == "jdtls" then
+      -- tell LSP to use `eclipse.formatter.xml` if exits in the workspace
+      local formatter_url = jdtls_formatter_url(args.buf, client)
+      if formatter_url then
+        client.settings = vim.tbl_deep_extend("force", client.settings or {}, {
+          java = {
+            format = {
+              settings = {
+                url = formatter_url,
+              },
+            },
+          },
+        })
+        client:notify("workspace/didChangeConfiguration", { settings = client.settings })
+      end
     end
     vim.keymap.set("n", "grd", vim.lsp.buf.definition, { desc = "vim.lsp.buf.definition" })
   end,
